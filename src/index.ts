21b01d14659e476359940cf3d5719c2b07eb0583
Xrok <sercar88@gmail.com>
2021-06-14 20:10:15 -0500

    fix fixed port to heroku

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,41 @@
-import express from "express";
-import mongoose from "mongoose";
-import { userRouter } from "./routes/user.route";
-import bodyParser from "body-parser";
-import { loggerMiddleware } from "./middleware/logger.middleware";
-import { coreRouter } from "./routes/core.route";
-import dotenv from "dotenv"
+import express from 'express';
+import mongoose from 'mongoose';
+import { userRouter } from './routes/user.route';
+import bodyParser from 'body-parser';
+import { loggerMiddleware } from './middleware/logger.middleware';
+import { coreRouter } from './routes/core.route';
+import dotenv from 'dotenv';
 
-dotenv.config()
+dotenv.config();
 
-const { NODE_ENV, MONGO_URL_ATLAS, MONOGO_URL_LOCAL,SERVER_PORT } = process.env;
+const { NODE_ENV, MONGO_URL_ATLAS, MONOGO_URL_LOCAL, SERVER_PORT } = process.env;
 
-const MONGO_URL =
-  NODE_ENV == "development" ? MONOGO_URL_LOCAL : MONGO_URL_ATLAS;
+const MONGO_URL = NODE_ENV == 'development' ? MONOGO_URL_LOCAL : MONGO_URL_ATLAS;
 
 const app = express();
 app.use(bodyParser.json());
 app.use(loggerMiddleware);
 
-app.get("/", (req, res) => {
-  res.send("Hello world!");
+app.get('/', (req, res) => {
+    res.send('Hello world!');
 });
-app.use(coreRouter)
-app.use("/user", userRouter);
-
-
+app.use(coreRouter);
+app.use('/user', userRouter);
 
 mongoose.connect(
     MONGO_URL,
-  {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  },
-  () => {
-    console.log("connected to database");
-  }
+    {
+        useCreateIndex: true,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    },
+    () => {
+        console.log('connected to database');
+    }
 );
 
 // start the Express server
-app.listen(SERVER_PORT, () => {
-  // tslint:disable-next-line:no-console
-  console.log(`server started at http://localhost:${SERVER_PORT}`);
+app.listen(process.env.PORT || SERVER_PORT, () => {
+    // tslint:disable-next-line:no-console
+    console.log(`server started at http://localhost:${process.env.PORT || SERVER_PORT}`);
 });
